5427f98f43c10ef6c475a2b0cc7f5c5d7ca82023
echavess <echavess@ucsc.edu>
2014-05-30 23:52:23 -0700

    Obspy scripts

---
+++ plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from obspy.core import read
+from obspy.imaging.spectrogram import spectrogram
+import numpy as np
+
+
+st = read('indi_saju.bhz.sac')
+print(st)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+spectrogram(st[0].data, st[0].stats.sampling_rate,wlen=5.,axes=ax,log=True)
+ax.set_ylim(0.1,3)
+ax.set_xlabel('Time [s]')
+ax.set_ylabel('Frequency [Hz]')
+fig.savefig('SPEC.ps')
+#plt.show()
+
+
